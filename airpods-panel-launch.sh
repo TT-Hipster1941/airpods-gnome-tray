b9605507a1bb8f13398f84a886f0ee716bc9420d
TT-Hipster1941 <npleau@protonmail.com>
2026-08-24 09:24:48 -0400

    Remove hardcoded home path, use $HOME instead

--- airpods-panel-launch.sh
+++ airpods-panel-launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if ! pgrep -f "airpods-panel.py" > /dev/null; then
-    /usr/bin/python3 /home/spike/.local/bin/airpods-panel.py &
+    /usr/bin/python3 "$HOME/.local/bin/airpods-panel.py" &
 fi
